daecdb1a8af0774380f6067a68839296c3972f31
krithi <gattikrithi@gmail.com>
2026-03-19 22:56:53 +0530

    retried

---
+++ tests/weekday1.js
@@ -0,0 +1,21 @@
+/*Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`, 
+including an uninitialized variable.*/
+
+//string
+let username="krithi"
+console.log(typeof username)
+
+//organization
+
+let organization="Google"
+console.log(typeof organization)
+//contactNumber
+let contactNumber=939399939
+console.log(typeof contactNumber)
+//knowsAutomation
+let knowsAutomation=true
+console.log(typeof knowsAutomation)
+
+//usesPlaywright
+let usesPlaywright
+console.log(usesPlaywright)
